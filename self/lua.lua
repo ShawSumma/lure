78f23f83ee0d83beb872c56a4ac2fe1c77c4369a
Shaw Summa <shawsumma@gmail.com>
2022-06-25 14:28:12 -0400

    more work parser

--- self/lua.lua
+++ self/lua.lua
@@ -1,5 +1,41 @@
 #lang lua
 
+
+local fun = {}
+
+function fun.compose(first, ...)
+    if first == nil then
+        return function(...)
+            return ...
+        end
+    end
+    local next = fun.compose(...)
+    return function(...)
+        return first(next(...))
+    end
+end
+
+function fun.unlist(list, arr)
+    local arr = arr or {}
+    while #list ~= 0 do
+        arr[#arr + 1] = list[1]
+        list = list[2]
+    end
+    return arr
+end
+
+function fun.join(arr)
+    local function more(low, high)
+        if low == high then
+            return arr[low]
+        else
+            local mid = (low + high) / 2
+            return more(low, math.floor(mid)) .. more(math.floor(mid + 1), high)
+        end
+    end
+    return more(1, #arr)
+end
+
 local parser = {}
 
 function parser.new(src)
@@ -26,9 +62,16 @@ end
 
 function parser.any(state, ok, err)
     if state.pos <= string.len(state.src) then
-        return ok(parser.advance(state), string.sub(state.src, state.pos, state.pos))
+        local chr = string.sub(state.src, state.pos, state.pos)
+        return ok(parser.advance(state, chr), chr)
     else
         return err(state, "unexpected: end of file")
+    end
+end
+
+function parser.accept(value)
+    return function(state, ok, err)
+        return ok(state, value)
     end
 end
 
@@ -83,10 +126,10 @@ end
 
 function parser.first(...)
     local function more(opts, start)
-        if start == #opts then
-            return opts[start]
-        end
         local first = opts[start]
+        if start == #opts then
+            return first
+        end
         local next = more(opts, start + 1)
         return function(state, ok, err)
             return first(state, ok, function(state2, msg1)
@@ -110,71 +153,114 @@ function parser.transform(next, func)
     end
 end
 
-function parser.list1(next)
+function parser.cons(parse1, parse2)
     return function(state, ok, err)
-        return next(state,
-            function(state, data1)
-                return parser.list1(next)(state,
-                    function(state, data2)
-                        return ok(state, {data1, data2})
-                    end,
-                    function(state2, msg)
-                        return ok(state, {data1, {}})
-                    end
-                )
+        return parse1(state, function(state, data1)
+            return parse2(state, function(state, data2)
+                return ok(state, {data1, data2})
             end,
-            err
-        )
+            err)
+        end,
+        err)
     end
 end
 
 function parser.list0(next)
-    local list1 = parser.list1(next);
-    return function(state, ok, err)
-        return list1(state, ok, function(state2, msg)return ok(state, {}) end)
+    local rest
+    local function more(state, ok, err)
+        return rest(state, ok, function(state2, msg)
+            return ok(state, {})
+        end)
     end
+    rest = parser.cons(next, more)
+    return more
 end
 
-local fun = {}
-
-function fun.compose(first, ...)
-    if first == nil then
-        return function(...)
-            return ...
-        end
-    end
-    local next = fun.compose(...)
-    return function(...)
-        return first(next(...))
-    end
+function parser.list1(next)
+    return parser.cons(next, parser.list0(next))
 end
 
-function fun.unlist(list)
-    local arr = {}
-    while #list ~= 0 do
-        arr[#arr + 1] = list[1]
-        list = list[2]
-    end
-    return arr
+
+function parser.skiponly(skip, read)
+    return parser.transform(parser.cons(skip, read), function(data) return data[2] end)
 end
 
-function fun.join(arr)
-    local function more(low, high)
-        if low == high then
-            return arr[low]
+function parser.skips(skip, read)
+    return parser.skiponly(parser.list0(skip), read)
+end
+
+function parser.string(str)
+    local ret = parser.accept({})
+    local len = string.len(str)
+    for i=0, len-1 do
+        ret = parser.cons(parser.exact(string.sub(str, len-i, len-i)), ret)
+    end
+    return parser.transform(ret, fun.compose(fun.join, fun.unlist))
+end
+
+function parser.listof(...)
+    local tab = {...}
+    local function more(head)
+        local cur = tab[head]
+        if cur == nil then
+            return parser.accept({})
         else
-            local mid = (low + high) / 2
-            return more(low, math.floor(mid)) .. more(math.floor(mid + 1), high)
+            return parser.cons(cur, more(head + 1))
         end
     end
-    return more(1, #arr)
+    return more(1)
+end
+
+function parser.ast(type, ...)
+    return parser.transform(
+        parser.listof(...),
+        function(data)
+            return fun.unlist(data, {type=type})
+        end
+    )
+end
+
+function parser.select(n, ...)
+    return parser.transform(parser.listof(...), function(data)
+        return data[n]
+    end)
+end
+
+local function aststr(ast)
+    if type(ast) == "table" then
+        local function more(acc, n)
+            if ast[n] == nil then
+                return acc
+            else
+                return more(acc .. " " .. aststr(ast[n]), n + 1)
+            end
+        end
+        return "(" .. ast.type .. more("", 1) .. ")"
+    else
+        return tostring(ast)
+    end
 end
 
 parser.lowerletter = parser.range('a', 'z')
 parser.upperletter = parser.range('A', 'Z')
 parser.digit = parser.range('0', '9')
 parser.letter = parser.first(parser.lowerletter, parser.upperletter)
-parser.digits = parser.transform(parser.list1(parser.digit), fun.compose(fun.join, fun.unlist))
+parser.digits = parser.transform(parser.list1(parser.digit), fun.compose(tonumber, fun.join, fun.unlist))
+parser.ident = parser.transform(
+    parser.cons(
+        parser.first(parser.letter, parser.exact('_')),
+        parser.list0(
+            parser.first(parser.digit, parser.letter, parser.exact('_'))
+        )
+    ),
+    fun.compose(fun.join, fun.unlist)
+)
+
+local lua = {}
+lua.number = parser.ast("number", parser.digits)
+lua.ident = parser.ast("ident", parser.ident)
+lua.single = parser.first(lua.number, lua.ident)
+lua.args = parser.
 
 -- parser.any(parser.new("h"), function(state, data) print(data) end, function(state, msg) print(msg) end)
-parser.digits(parser.new("123456"), function(state, data) print(data) end, function(state, msg) print(msg) end)
+lua.single(parser.new("122"), function(state, data) print(aststr(data)) end, function(state, msg) print(msg) end)
